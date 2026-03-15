4eecd41f90c844a47e556c31db4f81d6c8a8eb93
Karoliya Agin Doli <karoliyaagindoli22@gmail.com>
2026-03-15 23:22:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const orderRoutes = require("./orderRoutes")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
 app.use(express.json())
 
 app.use(express.static(__dirname));
